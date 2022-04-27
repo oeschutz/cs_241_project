0302586bb79820a5c9831533806842fe94170e35
oeschutz <89801416+oeschutz@users.noreply.github.com>
2022-04-27 13:31:20 -0700

    Add files via upload

--- work.py
+++ work.py
@@ -35,11 +35,11 @@ df = pandas.DataFrame.from_dict(csv_to_dict("ufos.csv.csv")) # dictonary to data
 
 plt.rcParams['figure.figsize'] = [20, 10]
 plt.rcParams['figure.dpi'] = 100
-plt.rcParams['axes.facecolor'] = 'xkcd:grey'
-plt.rcParams['figure.facecolor'] = 'xkcd:grey'
+plt.rcParams['axes.facecolor'] = '#5a7d9a'
+plt.rcParams['figure.facecolor'] = '#5a7d9a'
 plt.tick_params(axis='both', which='major', labelsize=10, labelbottom = True, 
                 bottom=False, top = False, left = False, labeltop=False)
 plt.style.use("seaborn-dark")
 
 p = seaborn.scatterplot(x = "longitudes", y = "latitudes", hue = "log(duration)", data = df, palette="rocket", size = "log(duration)", alpha = 0.75)
-p.get_figure().savefig("test_us15.png",bbox_inches='tight',transparent=False)
+p.get_figure().savefig("test_us16.png",bbox_inches='tight',transparent=False)
