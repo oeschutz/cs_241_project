76139906cd3d033d9da369104cff62b34d8cf8a0
oeschutz <89801416+oeschutz@users.noreply.github.com>
2022-04-27 13:23:28 -0700

    Add files via upload

--- work.py
+++ work.py
@@ -2,15 +2,10 @@
 
 #notes: removed ufo sightings with durations longer than 5 hours and shorter than 5 seconds. 
 
-# try to map onto map of us? idk ho, but maybe?
-# try to remove borders on points
 
 from math import log
 
-def csv_to_dict(file):
-    # 5 commas b4 duration
-    # 9 b4 lat
-    # 10 b4 long
+def csv_to_dict(file): # read csv into dictionary
     durs, lats, longs, commas = [],[],[],0
     for line in open(file):
         commas = 0
@@ -26,7 +21,7 @@ def csv_to_dict(file):
                 elif commas == 10:
                     lon += c
             lat, lon, dur = float(lat), float(lon), log(float(dur))
-            if -125 < lon < -67 and 25 < lat < 49:
+            if -125 < lon < -67 and 25 < lat < 49: # filter by contguous US latitude/longitude
                 durs.append(dur)
                 lats.append(lat)
                 longs.append(lon)
@@ -36,13 +31,15 @@ import pandas
 import seaborn
 import matplotlib.pyplot as plt
 
-df = pandas.DataFrame.from_dict(csv_to_dict("ufos.csv.csv"))
+df = pandas.DataFrame.from_dict(csv_to_dict("ufos.csv.csv")) # dictonary to dataframe
 
 plt.rcParams['figure.figsize'] = [20, 10]
 plt.rcParams['figure.dpi'] = 100
+plt.rcParams['axes.facecolor'] = 'xkcd:grey'
+plt.rcParams['figure.facecolor'] = 'xkcd:grey'
 plt.tick_params(axis='both', which='major', labelsize=10, labelbottom = True, 
                 bottom=False, top = False, left = False, labeltop=False)
-#plt.style.use("dark_background")
+plt.style.use("seaborn-dark")
 
-p = seaborn.scatterplot(x = "longitudes", y = "latitudes", hue = "log(duration)", data = df, palette="rocket", size = "log(duration)", alpha = 0.5)
-p.get_figure().savefig("test_us9.png",bbox_inches='tight',transparent=True)
+p = seaborn.scatterplot(x = "longitudes", y = "latitudes", hue = "log(duration)", data = df, palette="rocket", size = "log(duration)", alpha = 0.75)
+p.get_figure().savefig("test_us15.png",bbox_inches='tight',transparent=False)
